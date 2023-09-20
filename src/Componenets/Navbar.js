1ed42f4cc7d3f6a75cf7e263b0f8abd410c7e51d
AltPact <arikashley@gmail.com>
2023-09-20 21:07:08 +0100

    Added Navbar using Reducer

---
+++ src/Componenets/Navbar.js
@@ -0,0 +1,30 @@
+import { useReducer, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"
+import "../Styles/main.css";
+import clsx from "clsx";
+
+export default function Navbar() {
+    const [drawerOpen, toggleDrawer] = useReducer((currentState) => !currentState, false);
+
+    return (
+        <header>
+            <h3>Logo</h3>
+            <nav className={clsx(drawerOpen && "responsive_nav")}>
+                <a href="/#">Home</a>
+                <a href="/#">My Work</a>
+                <a href="/#">Blog</a>
+                <a href="/#">About me</a>
+                <button
+                    className="nav-btn nav-close-btn"
+                    onClick={toggleDrawer}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button
+                className="nav-btn"
+                onClick={toggleDrawer}>
+                <FaBars />
+            </button>
+        </header>
+    );
+}
